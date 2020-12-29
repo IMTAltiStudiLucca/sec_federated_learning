ba0dec6b4132e67e0b5c2475afb6a6773e31ef97
Simone <s.soderi@gmail.com>
2020-12-29 16:39:22 +0100

    Added some comments

--- src/chi_squared_test.py
+++ src/chi_squared_test.py
@@ -8,17 +8,19 @@
                  H0 (null hypothesis): the two classification criteria are independent of each other
                  H1: The two criteria are related to each other.
     Credits:  Statistical Methods for Machine Learning (book), J. Brownlee
+              https://machinelearningmastery.com/chi-squared-test-for-machine-learning/
 """
 
-# chi-squared test with similar proportions
 from scipy.stats import chi2_contingency
 from scipy.stats import chi2
 
 """ Contingency Table
-
-Given a data set (e.g., MNIST) and two classification criteria X and Y (e.g., might be the digits), a contingency table 
-is defined as a table showing the values of the frequencies of the values (BASELINES) of the data set with respect to 
-the different classes of criteria X and Y.
+Given a data set (e.g., MNIST) and two classification criteria X and Y (e.g., might be the digits: original + with a 
+bias), a contingency table is defined as a table showing the values of the frequencies of the values  of the data set 
+with respect to the different classes of criteria X and Y.
+Example:
+X = [digit-0  digit-0_with_fracture];
+Y = [digit-3  digit-3_with_black_corner];
 
 Each cell in the table corresponds to the count or frequency of observations that correspond to the row and column 
 categories.
@@ -32,8 +34,8 @@ stat, p, dof, expected = chi2_contingency(table)
 print('dof=%d' % dof)
 print(expected)
 
-# interpret test-statistic
-# Significance level indicates that with 95% the two classification criteria cannot be considered independent.
+""" Interpret test-statistic
+"""
 prob = 0.95
 critical = chi2.ppf(prob, dof)
 print('probability=%.3f, critical=%.3f, stat=%.3f' % (prob, critical, stat))
@@ -42,7 +44,12 @@ if abs(stat) >= critical:
 else:
     print('Independent (fail to reject H0)')
 
-# interpret p-value
+
+""" Interpret p-value
+The p value helps to understand if the difference between the observed result and the hypothesized one 
+is due to the randomness introduced by the sampling, or if this difference is statistically significant, that is, 
+difficult to explain through the randomness due to the sampling.
+"""
 alpha = 1.0 - prob
 print('significance=%.3f, p=%.3f' % (alpha, p))
 if p <= alpha:
