40c413eb4c62e1f2c9e940484798426423570f64
Gabriele <gabriele.costa@imtlucca.it>
2021-01-28 17:54:11 +0100

    test script ready

--- score-test.sh
+++ score-test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-for i in `seq 1 5`; do
+for i in `seq 1 10`; do
   echo Launching container N. $i
-  docker run -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-score > "score-${i}.out" &
+  docker run -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label >> "score-${i}.out" 2>&1 &
+  wait
 done
-
-wait
 echo Completed
