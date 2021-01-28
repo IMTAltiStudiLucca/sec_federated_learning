4f83f558b47806f1b329af3ea7f4b9ec3fafc399
Gabriele <gabriele.costa@imtlucca.it>
2021-01-28 12:48:11 +0100

    test script ready

--- label-test.sh
+++ label-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 0 2`; do
+for i in `seq 1 10`; do
   echo Launching container N. $i
   docker run -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label > "label-${i}.out" &
 done
