b53b48010cbf00fc08860613950813f46e6d3ba6
Fabio Pinelli <fabio.pinelli@gmail.com>
2021-01-29 17:43:30 +0100

    parallel test

--- label-test.sh
+++ label-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in `seq 1 10`; do
+for i in `seq 1 3`; do
   echo Launching container N. $i
   docker run -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label >> "label-${i}.out" 2>&1 &
-  wait
 done
+wait
 echo Completed
