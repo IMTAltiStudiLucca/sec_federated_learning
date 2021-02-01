e9eccaaca7790d6649ff9cf9e0e5b78aa517ef70
Gabriele <gabriele.costa@imtlucca.it>
2021-02-01 15:47:14 +0100

    new test script for label

--- label-test.sh
+++ label-test.sh
@@ -2,7 +2,7 @@
 
 for i in `seq 1 6`; do
   echo Launching container N. $i
-  docker run --cpus 1 -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label >> "label-${i}.out" 2>&1 &
+  docker run --cpuset-cpus $((i)) -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label >> "label-${i}.out" 2>&1 &
 done
 wait
 echo Completed
