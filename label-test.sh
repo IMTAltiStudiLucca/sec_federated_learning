93f2100f2e4bf071a3297c7722649b5257a0145c
Gabriele <gabriele.costa@imtlucca.it>
2021-02-01 16:04:14 +0100

    new test script for label

--- label-test.sh
+++ label-test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for i in `seq 1 6`; do
+if [[ $1 -gt $(nproc) ]]
+then
+  echo "Not enough CPUs"
+  exit
+fi
+
+for i in `seq 0 $((i - 1))`; do
   echo Launching container N. $i
   docker run --cpuset-cpus $((i)) -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test-label >> "label-${i}.out" 2>&1 &
 done
