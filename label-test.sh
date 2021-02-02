4c0329aa4bd8e87cb31977d04a1774cfe1a916ea
Gabriele <gabriele.costa@imtlucca.it>
2021-02-02 18:42:21 +0100

    new test script

--- label-test.sh
+++ label-test.sh
@@ -10,8 +10,9 @@ for i in `seq 0 $(($1 - 1))`; do
   index=$(($2 + i))
   setup="./simulations/simulazione_${index}.yaml"
   mkdir -p "./${index}"
+  cd "./${index}"
   echo Launching container N. $i with setup $setup
-  docker run --cpuset-cpus $((i)) --env script=label-attack.py --env setup=$setup -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test >> "./${index}/simulazione_${index}.out" 2>&1 &
+  docker run --cpuset-cpus $((i)) --env script=label-attack.py --env setup=$setup -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test >> "simulazione_${index}.out" 2>&1 &
 done
 wait
 echo Completed
