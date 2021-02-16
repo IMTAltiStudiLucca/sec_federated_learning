543636b4955a9724955544cec637faccd08e9f37
Gabriele <gabriele.costa@imtlucca.it>
2021-02-16 20:50:58 +0100

    cpu offser

--- multi-test.sh
+++ multi-test.sh
@@ -14,7 +14,7 @@ for i in `seq 0 $(($1 - 1))`; do
   mkdir -p "./${index}"
   cd "./${index}"
   echo Launching container N. $i with setup $setup
-  docker run --cpuset-cpus $((index % proc)) --env script=label-attack-multichannel.py --env setup=$setup -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test >> "simulazione_${index}.out" 2>&1 &
+  docker run --cpuset-cpus $(((i + $3) % proc)) --env script=label-attack-multichannel.py --env setup=$setup -v $(pwd):/home/fedexp/src/tests gabrielec/fedexp-test >> "simulazione_${index}.out" 2>&1 &
   cd ..
 done
 wait
